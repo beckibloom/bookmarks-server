c1e884f16dbf54f7f9e13bed80af6ac93aa2ba6e
Becki Blumer <beckiblumer@Beckis-MacBook-Pro.local>
2019-08-18 20:48:33 -0500

    moved test seed data to the test file and all tests are passing

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL,
+    TEST_DB_URL: process.env.TEST_DB_URL,
     API_KEY: process.env.API_KEY,
 }
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -1,15 +1,48 @@
 const { expect } = require('chai')
 const knex = require('knex')
+const { TEST_DB_URL, API_KEY } = require('../config')
 const app = require('../src/app')
-const { makeBookmarksArray } = require('./bookmarks.fixtures')
+// const { makeBookmarksArray } = require('./bookmarks.fixtures.js')
 
-describe.only('Bookmarks Endpoints', function() {
+// Was not able to get bookmarks.fixtures.js to work - kept getting error 'makeBookmarksArray' is not a function and could not figure out another work-around
+const bookmarksArray = [
+  {
+    id: 1,
+    title: 'First bookmark title',
+    url: 'www.heresabookmark.com',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+    rating: 4
+  },
+  {
+    id: 2,
+    title: 'Second bookmark title',
+    url: 'www.heresasecondbookmark.com',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+    rating: 3
+  },
+  {
+    id: 3,
+    title: 'Third bookmark title',
+    url: 'www.heresathirdbookmark.com',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+    rating: 1
+  },
+  {
+    id: 4,
+    title: 'Fourth bookmark title',
+    url: 'www.heresafourthbookmark.com',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+    rating: 5
+  },
+]
+
+describe('Bookmarks Endpoints', function() {
   let db
 
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: 'postgresql://dunder-mifflin@localhost/bookmarks-test',
+      connection: TEST_DB_URL,
     })
     app.set('db', db)
   })
@@ -25,12 +58,14 @@ describe.only('Bookmarks Endpoints', function() {
       it(`responds with 200 and an empty list`, () => {
         return supertest(app)
           .get('/bookmarks')
+          .set('Authorization', `Bearer ${API_KEY}`)
           .expect(200, [])
       })
     })
 
     context('Given there are bookmarks in the database', () => {
-      const testBookmarks = makeBookmarksArray()
+      //changed this code to include bookmarksArray variable rather than using the function from the fixtures file
+      const testBookmarks = bookmarksArray
   
       beforeEach('insert bookmarks', () => {
         return db
@@ -41,6 +76,7 @@ describe.only('Bookmarks Endpoints', function() {
       it('responds with 200 and all of the bookmarks', () => {
         return supertest(app)
           .get('/bookmarks')
+          .set('Authorization', `Bearer ${API_KEY}`)
           .expect(200, testBookmarks)
       })
     })
@@ -52,12 +88,14 @@ describe.only('Bookmarks Endpoints', function() {
         const bookmarkId = 123456
         return supertest(app)
           .get(`/bookmarks/${bookmarkId}`)
+          .set('Authorization', `Bearer ${API_KEY}`)
           .expect(404, { error: { message: `Bookmark doesn't exist` } })
       })
     })
 
     context('Given there are bookmarks in the database', () => {
-      const testBookmarks = makeBookmarksArray()
+      //changed this code to include bookmarksArray variable rather than using the function from the fixtures file
+      const testBookmarks = bookmarksArray
 
       beforeEach('insert bookmarks', () => {
         return db
@@ -70,6 +108,7 @@ describe.only('Bookmarks Endpoints', function() {
         const expectedBookmark = testBookmarks[bookmarkId - 1]
         return supertest(app)
           .get(`/bookmarks/${bookmarkId}`)
+          .set('Authorization', `Bearer ${API_KEY}`)
           .expect(200, expectedBookmark)
       })
     })
